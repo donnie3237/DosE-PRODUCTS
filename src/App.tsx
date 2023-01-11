86aeb30cd6ead571fed14fc573a8ba3bc281fda5
paradorn3237 <paradorn3237@gmail.com>
2023-01-11 18:24:15 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,13 @@ function App() {
        <Headers/>
        </div>
        <Routes>
+          <Route path='/' element={<Homepage/>}>
+            <Route path='/' element={<DosELIB/>}/>
+            <Route path='doselib' element={<DosELIB/>}/>
+            <Route path='dosecrud' element={<DosECRUD/>}/>
+            <Route path='doseml' element={<DosEML/>}/>
+            <Route path='doseiot' element={<DosEIOT/>}/>
+          </Route>
           <Route path='/products' element={<Homepage/>}>
              <Route path='' element={<DosELIB/>}/>
              <Route path='doselib' element={<DosELIB/>}/>
