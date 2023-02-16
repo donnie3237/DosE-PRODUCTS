0a4a1299df63ac4c01d1c47e86ac8999de803698
SoRa <paradorn3237@gmail.com>
2023-02-16 17:57:29 +0700

    .

--- src/components/HomePage/DosE-DOSE3/Dose3.tsx
+++ src/components/HomePage/DosE-DOSE3/Dose3.tsx
@@ -2,7 +2,38 @@ import React from 'react'
 import './dose3.scss'
 function Dose3() {
   return (
-    <div className='dose3'>Dose3</div>
+    <div className='dose3 flex'>
+      <div className="img flex">
+      <img src={require('./logo.png')} alt="" />
+      </div>
+      <div className="ins">
+        -installain
+      </div>
+      <div className="therminal">
+        <p>npm i -g dose3</p>
+      </div>
+      <div className="therminal">
+        <p>pnpm i -g dose3</p> 
+      </div>
+      <div className="therminal">
+        <p>yarn add -g dose3</p> 
+      </div>
+      <div className="ins">
+        -useage
+      </div>
+      <div className="topic flex">
+        anything what you want
+      </div>
+      <div className="contents contents2">
+        <div className="box">Front-End</div>
+        <div className="box">Back-End</div>
+        <div className="box">Mobile APP</div>
+        <div className="box">Desktop APP</div>
+      </div>
+      <div className="req">
+        <h1>Require</h1>
+      </div>
+    </div>
   )
 }
 
